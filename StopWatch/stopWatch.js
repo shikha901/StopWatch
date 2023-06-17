c3c0c209edf43e54d305cc3ddb2b3cf8b8a9bb1b
shikha901 <136499979+shikha901@users.noreply.github.com>
2023-06-17 14:37:06 +0530

    StopWatch added all 3 files

---
+++ StopWatch/stopWatch.js
@@ -0,0 +1,93 @@
+
+//  Variables
+var startTimerButton = document.querySelector('.startTimer');
+var pauseTimerButton = document.querySelector('.pauseTimer');
+var timerDisplay = document.querySelector('.timer');
+var startTime;
+var updatedTime;
+var difference;
+var tInterval;
+var savedTime;
+var paused = 0;
+var running = 0;
+
+//Start watch function
+
+
+function startTimer(){
+  if(!running){
+    startTime = new Date().getTime();
+    tInterval = setInterval(getShowTime, 1);
+    paused = 0;
+    running = 1;
+    timerDisplay.style.background = "#0d095a";
+    timerDisplay.style.cursor = "auto";
+    timerDisplay.style.color = "yellow";
+    startTimerButton.classList.add('lighter');
+    pauseTimerButton.classList.remove('lighter');
+    startTimerButton.style.cursor = "auto";
+    pauseTimerButton.style.cursor = "pointer";
+  }
+}
+
+//Pause watch function
+
+function pauseTimer(){
+  if (!difference){
+    // if timer never started, don't allow pause button to do anything
+  } else if (!paused) {
+    clearInterval(tInterval);
+    savedTime = difference;
+    paused = 1;
+    running = 0;
+    timerDisplay.style.background = "#0b1544";
+    timerDisplay.style.color = "white";
+    timerDisplay.style.cursor = "pointer";
+    startTimerButton.classList.remove('lighter');
+    pauseTimerButton.classList.add('lighter');
+    startTimerButton.style.cursor = "pointer";
+    pauseTimerButton.style.cursor = "auto";
+  } else {
+    startTimer();
+  }
+}
+
+
+//Reset Timer function
+function resetTimer(){
+  clearInterval(tInterval);
+  savedTime = 0;
+  difference = 0;
+  paused = 0;
+  running = 0;
+  timerDisplay.innerHTML = 'Start Again!';
+  timerDisplay.style.background = "#A90000";
+  timerDisplay.style.color = "#fff";
+  timerDisplay.style.cursor = "pointer";
+  startTimerButton.classList.remove('lighter');
+  pauseTimerButton.classList.remove('lighter');
+  startTimerButton.style.cursor = "pointer";
+  pauseTimerButton.style.cursor = "auto";
+}
+
+
+//Running watch timer Display function
+function getShowTime(){
+  updatedTime = new Date().getTime();
+  if (savedTime){
+    difference = (updatedTime - startTime) + savedTime;
+  } else {
+    difference =  updatedTime - startTime;
+  }
+  // var days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((difference % (1000 * 60)) / 1000);
+  var milliseconds = Math.floor((difference % (1000 * 60)) / 100);
+
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+  milliseconds = (milliseconds < 100) ? (milliseconds < 10) ? "00" + milliseconds : "0" + milliseconds : milliseconds;
+  timerDisplay.innerHTML = hours + ':' + minutes + ':' + seconds + ':' + milliseconds;
+}
